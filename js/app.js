a751d260c00bf70667d933c6c2385258a5184038
w181496 <w181496@gmail.com>
2015-12-02 17:56:33 +0800

    modify css

--- js/app.js
+++ js/app.js
@@ -3,6 +3,15 @@ window.addEventListener("load", function() {
   start();
 });
 
+function result(str, type) {
+   if(type == 0) {
+      $("#num").attr("class","ui input fluid error");
+   } else {
+      $("#num").attr("class","ui input fluid");
+   }
+   alert(str);
+}
+
 function start() {
 
    $('#about').click(function(event) {
@@ -98,20 +107,20 @@ function start() {
             output.innerHTML = "";
             //alert(msg);
             console.log(msg);
-            if(msg == "12") alert("可能中特別獎，請輸入完整號碼!");
-            else if(msg == "11") alert("可能中特獎，請輸入完整號碼!");
-            else if(msg == "10") alert("可能中頭獎，請輸入完整號碼!");
-            else if(msg == "9") alert("恭喜中特別獎!");
-            else if(msg == "8") alert("恭喜中特獎!");
-            else if(msg == "7") alert("恭喜中增開六獎!");
-            else if(msg == "6") alert("恭喜中六獎!");
-            else if(msg == "5") alert("恭喜中五獎!");
-            else if(msg == "4") alert("恭喜中四獎!");
-            else if(msg == "3") alert("恭喜中三獎!");
-            else if(msg == "2") alert("恭喜中二獎!");
-            else if(msg == "1") alert("恭喜中頭獎!");
-            else if(msg == "-1") alert("輸入格式錯誤!");
-            else output.innerHTML = "沒中G_G";
+            if(msg == "12") result("可能中特別獎，請輸入完整號碼!", 1);
+            else if(msg == "11") result("可能中特獎，請輸入完整號碼!", 1);
+            else if(msg == "10") result("可能中頭獎，請輸入完整號碼!", 1);
+            else if(msg == "9") result("恭喜中特別獎!", 1);
+            else if(msg == "8") result("恭喜中特獎!", 1);
+            else if(msg == "7") result("恭喜中增開六獎!", 1);
+            else if(msg == "6") result("恭喜中六獎!", 1);
+            else if(msg == "5") result("恭喜中五獎!", 1);
+            else if(msg == "4") result("恭喜中四獎!", 1);
+            else if(msg == "3") result("恭喜中三獎!", 1);
+            else if(msg == "2") result("恭喜中二獎!", 1);
+            else if(msg == "1") result("恭喜中頭獎!", 1);
+            else if(msg == "-1") result("輸入格式錯誤!", 0);
+            else result("沒中G_G", 1);
          },
 
          error:function(xhr, ajaxOptions, thrownError){
